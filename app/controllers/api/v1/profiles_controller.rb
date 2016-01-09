dba11bd0cdd3713946666e6b06b6713c4797ee1c
Harikesh Kolekar <harikeshkolekar@gmail.com>
2016-01-09 20:05:42 +0530

    notification @pagintion added for profile

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -2,10 +2,11 @@ class Api::V1::ProfilesController < Api::V1::ApiController
 	def index
 		updated_date = Date.strptime(params[:update_at],"%d/%m/%Y %H:%M:%S:%L") - 10.second rescue nil
 		if updated_date.blank?
-			@profiles = User.profile#.paginate(:page => params[:page])
+			@profiles = User.profile.paginate(:page => params[:page])
 		else
-			@profiles = User.profile.where("updated_at>?",updated_date)#.paginate(:page => params[:page]) 
+			@profiles = User.profile.where("updated_at>?",updated_date).paginate(:page => params[:page]) 
 		end
+		@notification_count = Notification.joins(:designations).where("designations.name='#{@user.designation}'").count
 	end
 	
 end
--- app/views/api/v1/profiles/index.json.rabl
+++ app/views/api/v1/profiles/index.json.rabl
@@ -1,4 +1,6 @@
-
+node(:number_of_pages) { @profiles.total_pages }
+node(:number_of_profiles) { @profiles.total_entries }
+node(:number_of_notifications) { @notification_count }
 child @profiles do
 	attributes :id, :name, :designation, :mobile_no1, :mobile_no2, :other_info, :home_district, :posting_district, :email, :lat, :long
 		node(:date_of_birth) { |m| date_to_string(m.date_of_birth)}
