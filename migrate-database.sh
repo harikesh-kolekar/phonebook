c0f693760e917bc0146af9dc82f9c69cbb729659
jagadalerj <jagadalerupchand5399@gmail.com>
2018-01-20 14:19:05 +0530

    remove migration

--- migrate-database.sh
+++ migrate-database.sh
@@ -3,3 +3,5 @@ set -e
 
 echo "Executing bundle exec 'rake db:migrate' ..."
 RAILS_ENV=production bundle exec rake db:migrate
+echo "Executing bundle exec 'rake db:seed' ..."
+RAILS_ENV=production bundle exec rake db:seed
