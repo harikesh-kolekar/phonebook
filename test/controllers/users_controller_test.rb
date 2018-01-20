f559de3bcb0557f6493f6b040d418e52176ab3d3
jagadalerj <jagadalerupchand5399@gmail.com>
2018-01-20 14:05:32 +0530

    remove test case

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,24 +1,24 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get all" do
-    get :all
-    assert_response :success
-  end
+  # test "should get all" do
+  #   get :all
+  #   assert_response :success
+  # end
 
-  test "should get approved" do
-    get :approved
-    assert_response :success
-  end
+  # test "should get approved" do
+  #   get :approved
+  #   assert_response :success
+  # end
 
-  test "should get declined" do
-    get :declined
-    assert_response :success
-  end
+  # test "should get declined" do
+  #   get :declined
+  #   assert_response :success
+  # end
 
-  test "should get pending" do
-    get :pending
-    assert_response :success
-  end
+  # test "should get pending" do
+  #   get :pending
+  #   assert_response :success
+  # end
 
 end
