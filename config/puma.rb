c08a4d4d3cfdc136acf663284905077f3ad37c51
harikesh-kolekar <harikeshkolekarr@gmail.com>
2019-05-11 12:03:01 +0530

    puma.rb updated

--- config/puma.rb
+++ config/puma.rb
@@ -1,13 +1,25 @@
 return if Rails.env.test? || Rails.env.development? rescue nil
-
-app_path = '/home/deployer/www/project'
-
-directory "#{app_path}/current"
-rackup "#{app_path}/current/config.ru"
-daemonize true
-pidfile "#{app_path}/shared/tmp/pids/puma.pid"
-state_path "#{app_path}/shared/tmp/puma.state"
 workers 2
-threads 6, 6
-bind "unix:#{app_path}/shared/tmp/sockets/puma.sock"
-preload_app!
+threads 1, 6
+
+app_path = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/"
+rails_env = ENV['RAILS_ENV'] || "production"
+environment rails_env
+
+# Set up socket location
+bind "unix://#{shared_dir}/sockets/puma.sock"
+# Logging
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+
+# Set master PID and state locations
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+activate_control_app
+
+on_worker_boot do
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+end
